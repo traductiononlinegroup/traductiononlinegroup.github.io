1a93b9e64da687cb044e5d41919c2d2397b8ebb9
traductiononlinegroup <traduction-online-group@hotmail.com>
2024-10-20 09:34:47 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,34 +8,12 @@ document.querySelector('button#go-to-top').addEventListener('click', () => {
     document.querySelector('#page-1').scrollIntoView(true)
 })
 
-document.querySelector('button#send').addEventListener('click', () => {
-    const name = document.querySelector('#name').value
-    const email = document.querySelector('#email').value
-    const message = document.querySelector('#message').value
-    console.log(`${name}\n${email}\n${message}`)
-    document.querySelector('#name').value = ''
-    document.querySelector('#email').value = ''
-    document.querySelector('#message').value = ''
-})
-
-// function isInViewPort(elem) {
-//     const rect = elem.getBoundingClientRect()
-//     return (
-//         rect.top >= 0
-//         && rect.left >= 0
-//         && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
-//         && rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-//     )
-// }
-
-// function checkVisibility() {
-//     console.log('page scroll')
-//     const page1 = document.querySelector('#page-1')
-//     if (isInViewPort(page1)){
-//         console.log('visible')
-//     } else {
-//         console.log('not visible')
-//     }
-// }
-
-// window.addEventListener('scroll', (e) => {console.log(e)})
+// document.querySelector('button#send').addEventListener('click', () => {
+//     const name = document.querySelector('#name').value
+//     const email = document.querySelector('#email').value
+//     const message = document.querySelector('#message').value
+//     console.log(`${name}\n${email}\n${message}`)
+//     document.querySelector('#name').value = ''
+//     document.querySelector('#email').value = ''
+//     document.querySelector('#message').value = ''
+// })
